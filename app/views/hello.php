a561bace44ecced93fb2325dc195634274eeeb0a
ashanwijenayake <ashan.wijenayake@gmail.com>
2015-05-16 22:33:25 +0530

    laravel framework

---
+++ app/views/hello.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<title> Welcome </title>
+	<style type="text/css">
+	.class1{
+		margin: 0 auto;
+		margin-top: 150px;
+		width: 290px;
+	}
+	</style>
+</head>
+<body>	
+		<div class="class1">
+			<h1> Welcome to Laravel</h1>
+		</div>
+</body>
+</html>
